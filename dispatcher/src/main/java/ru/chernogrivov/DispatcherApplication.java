5c33e306294fb3fb28647358528faf70ab90d044
MaximChernogrivov <frizyfrime@gmail.com>
2023-04-18 13:05:34 +0300

    add settings for bot

--- dispatcher/src/main/java/ru/chernogrivov/DispatcherApplication.java
+++ dispatcher/src/main/java/ru/chernogrivov/DispatcherApplication.java
@@ -2,8 +2,6 @@ package ru.chernogrivov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.chernogrivov.config.TelegramBotConfig;
-import ru.chernogrivov.controller.TelegramBot;
 
 @SpringBootApplication
 public class DispatcherApplication {
--- dispatcher/src/main/java/ru/chernogrivov/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/chernogrivov/controller/TelegramBot.java
@@ -1,18 +1,13 @@
 package ru.chernogrivov.controller;
-import jakarta.annotation.PostConstruct;
 
 import lombok.extern.log4j.Log4j;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 
 @Log4j
 @Component
