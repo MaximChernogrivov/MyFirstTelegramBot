1731e47fdbbc39599178190cdf89258b9f8d6c13
MaximChernogrivov <frizyfrime@gmail.com>
2023-04-21 14:13:43 +0300

    add config for Rabbitmq

---
+++ node/src/main/java/ru/chernogrivov/service/Impl/ConsumerServiceImpl.java
@@ -0,0 +1,46 @@
+package ru.chernogrivov.service.Impl;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.chernogrivov.service.ConsumerService;
+import ru.chernogrivov.service.ProducerService;
+
+import static ru.chernogrivov.models.RabbitQueue.*;
+
+@Service
+@Log4j
+public class ConsumerServiceImpl implements ConsumerService {
+
+    private final ProducerService producerService;
+
+    public ConsumerServiceImpl(ProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @Override
+    @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
+    public void consumeTextMessageUpdates(Update update) {
+        log.debug("NODE: has message");
+
+        var message = update.getMessage();
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("Hello from Node");
+        producerService.producerAnswer(sendMessage);
+    }
+
+    @Override
+    @RabbitListener(queues = DOC_MESSAGE_UPDATE)
+    public void consumeDocMessageUpdates(Update update) {
+        log.debug("NODE: has doc message");
+    }
+
+    @Override
+    @RabbitListener(queues = PHOTO_MESSAGE_UPDATE)
+    public void consumePhotoMessageUpdates(Update update) {
+        log.debug("NODE: has photo message");
+    }
+}
---
+++ node/src/main/java/ru/chernogrivov/service/Impl/ProducerServiceImpl.java
@@ -0,0 +1,24 @@
+package ru.chernogrivov.service.Impl;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.chernogrivov.service.ProducerService;
+
+import static ru.chernogrivov.models.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class ProducerServiceImpl implements ProducerService {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProducerServiceImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void producerAnswer(SendMessage sendMessage) {
+        rabbitTemplate.convertAndSend(ANSWER_MESSAGE, sendMessage);
+    }
+}
